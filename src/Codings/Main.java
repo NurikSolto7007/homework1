2d8b054d4fa7cbefbce598362caf9f18fc756c23
NurikSolto7007 <n.soltoev@gmail.com>
2019-12-27 21:56:36 +0600

    Initial commit

---
+++ src/Codings/Main.java
@@ -0,0 +1,10 @@
+package Codings;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final String name = "Hello";
+        String s = " world";
+        System.out.println(name + s);
+    }
+}
